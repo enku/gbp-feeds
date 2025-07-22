dc4bf371d0e77e9c6a4a9f8f818d28db2c634273
Albert Hopkins <marduk@letterboxes.org>
2025-07-22 16:55:46 -0500

    noxfile: re-mask Python 3.14
    There is an issue with python-dispatch:
    
    ```
        from pydispatch.dispatch import *
      File "/var/lib/jenkins/workspace/gbp-feeds/.nox/tests-3-14/lib/python3.14/site-packages/pydispatch/dispatch.py", line 10, in <module>
        from pydispatch.aioutils import AioWeakMethodContainer, AioEventWaiters
      File "/var/lib/jenkins/workspace/gbp-feeds/.nox/tests-3-14/lib/python3.14/site-packages/pydispatch/aioutils.py", line 4, in <module>
        from _weakrefset import _IterationGuard
    ImportError: cannot import name '_IterationGuard' from '_weakrefset' (/usr/lib/python3.14/_weakrefset.py)
    ```
    
    This project is possibly no longer being maintained, which means we may
    need to make adjustments.

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,14 @@
 # pylint: disable=missing-docstring
 import nox
 
+PYTHON_VERSIONS = (
+    "3.12",
+    "3.13",
+    # "3.14",  # python-dispatch does not (yet) work with this
+)
 
-@nox.session(python=("3.12", "3.13", "3.14"))
+
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
         "dev"
